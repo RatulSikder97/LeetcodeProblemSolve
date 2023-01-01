68aa6b3b39201d0f6a3dbbffd512c7c5131e7c4d
ratul.sikder <ratul.sikder@rokomari.com>
2023-01-01 09:54:47 +0600

    Optimized

--- ArrayConcat/ArrayConcat.java
+++ ArrayConcat/ArrayConcat.java
@@ -3,12 +3,10 @@ package ArrayConcat;
 public class ArrayConcat {
     class Solution {
         public int[] getConcatenation(int[] nums) {
-            int arrLen = nums.length;
-            int[] ans = new int[2*arrLen];
+            int[] ans = new int[2*nums.length];
 
-            for(int i=0;i<arrLen;i++) {
-                ans[i] = nums[i];
-                ans[arrLen+i] = nums[i];
+            for(int i=0;i<nums.length;i++) {
+                ans[i] = ans[arrLen+i] = nums[i];
             }
 
             return ans;
